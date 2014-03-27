586ed447728d410a421717ba50c56a650d56148d
OtakuNeko <ryulord1@embarqmail.com>
2014-03-26 20:43:32 -0400

    Auto level generation 1.0
    complete auto generation script version 1

--- Rhythm Siege/Assets/Scripts/SongAnalyze.cs
+++ Rhythm Siege/Assets/Scripts/SongAnalyze.cs
@@ -7,7 +7,7 @@ public class SongAnalyze : MonoBehaviour {
 	static float MULTIPLIER = 1.5f;
 
 	bool done = false;
-	float[] samples = new float[1024];
+	//float[] samples = new float[1024];
 	float[] spectrum = new float[1024];
 	float[] lastSpectrum = new float[1024];
 	ArrayList spectralFlux = new ArrayList( );
@@ -23,62 +23,97 @@ public class SongAnalyze : MonoBehaviour {
 	void Update () {
 		if (this.audio.isPlaying)
 		{
-			this.audio.GetSpectrumData (samples, 0, FFTWindow.Hamming);
 			spectrum.CopyTo(lastSpectrum, 0);
-			samples.CopyTo(spectrum, 0);
+			this.audio.GetSpectrumData (spectrum, 0, FFTWindow.Hamming);
 
-			float flux = 0;
-			for( int i = 0; i < spectrum.Length; i++ )	
+			float[] flux = {0,0,0};
+			for( int i = 0; i < 11; i++ )	
 			{
 				float value = (spectrum[i] - lastSpectrum[i]);
-				flux += value < 0? 0: value;
+				flux[0] += value < 0? 0: value;
+			}
+			for( int i = 11; i < 93 ; i++ )	
+			{
+				float value = (spectrum[i] - lastSpectrum[i]);
+				flux[1] += value < 0? 0: value;
+			}
+			for( int i = 93; i < spectrum.Length; i++ )	
+			{
+				float value = (spectrum[i] - lastSpectrum[i]);
+				flux[2] += value < 0? 0: value;
 			}
 			spectralFlux.Add(flux);
+
 		}
 
 		else if(done == false)
 		{
+
 			for( int i = 0; i < spectralFlux.Count; i++ )
 			{
-				int start = Mathf.Max( 0, i - THRESHOLD_WINDOW_SIZE );
-				int end = Mathf.Min( spectralFlux.Count - 1, i + THRESHOLD_WINDOW_SIZE );
-				float mean = 0;
-				for( int j = start; j <= end; j++ )
-				mean +=  (float)spectralFlux[j];
-				mean /= (end - start);
-				threshold.Add( mean * MULTIPLIER );
+				float[] mean = {0,0,0};
+				for(int a = 0; a<3; a++)
+				{
+					int start = Mathf.Max( 0, i - THRESHOLD_WINDOW_SIZE );
+					int end = Mathf.Min( spectralFlux.Count - 1, i + THRESHOLD_WINDOW_SIZE );
+					for( int j = start; j <= end; j++ ){
+						float[]temp = (float[])spectralFlux[j];
+						mean[a] +=  temp[a];}
+					mean[a] /= (end - start);
+					mean[a] *=  MULTIPLIER;
+				}
+				threshold.Add(mean);
 			}
+
 
 			for( int i = 0; i < threshold.Count; i++ )
 			{
-				if( (float)threshold[i] <=  (float)spectralFlux[i]){
-					prunnedSpectralFlux.Add( (float)spectralFlux[i] - (float)threshold[i] );
+				float[] temp3 = {0,0,0};
+				for(int a = 0; a<3; a++){
+					float[] temp1 = (float[])threshold[i];
+					float[] temp2 = (float[])spectralFlux[i];
+
+					if( temp1[a] <=  temp2[a]){
+						temp3[a] = ( temp2[a] - temp1[a] );
+					}
+					else{
+						temp3[a] = ( (float)0 );
+					}
 				}
-				else{
-					prunnedSpectralFlux.Add( (float)0 );
-				}
+				prunnedSpectralFlux.Add(temp3);
 			}
 			threshold.Clear();
+
 			for( int i = 0; i < prunnedSpectralFlux.Count - 1; i++ )
 			{
-				if( (float)prunnedSpectralFlux[i] > (float)prunnedSpectralFlux[i+1] ){
-					peaks.Add( (float)prunnedSpectralFlux[i]);}
-				else {
-					peaks.Add( (float)0 );	}			
+				float[] temp3 = {0,0,0};
+				for(int a = 0; a<3; a++){
+					float[] temp1 = (float[])prunnedSpectralFlux[i];
+					float[] temp2 = (float[])prunnedSpectralFlux[i+1];
+					if( temp1[a] > temp2[a] ){
+						temp3[a] = ( temp1[a]);}
+					else {
+						temp3[a]=( (float)0 );	}	
+					}
+				peaks.Add(temp3);
 			}
 			prunnedSpectralFlux.Clear();
-
+			
 			StreamWriter noteWriter = new StreamWriter(this.audio.clip.name);
-			for( int i = 0; i < peaks.Count; i += 5 )
+			for( int i = 0; i < peaks.Count; i += 25 )
 			{
 				float rate = 1024f/44100f;
 				float time = (float)i * rate;
-				if((float)peaks[i] != 0 )
-				{
-					Debug.Log(time);
-					noteWriter.WriteLine(time);
-					//noteWriter.WriteLine((float)peaks[i]);
-					noteWriter.WriteLine("0");
+				float[] temp = (float[])peaks[i];
+				for(int a = 0; a<3;a++){
+
+					if(temp[a] != 0 )
+					{
+						Debug.Log(time);
+						noteWriter.WriteLine(time);
+						//noteWriter.WriteLine((float)peaks[i]);
+						noteWriter.WriteLine(a);
+					}
 				}
 			}
 			noteWriter.Close();
