ccc9bcef9fbe6370c84f9131fbacf03e23d4d579
David Kops <kopsdavid@hotmail.com>
2014-05-08 23:32:12 -0400

    Main Menu
    Added background, moving clouds, lightning, and menu buttons in the
    style of Sketchboard_1.
    Some asset organization.

---
+++ Rhythm Siege/Assets/Scripts/Lightning.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lightning : MonoBehaviour {
+
+	public float lifeSeconds;
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(lifeSeconds <= 0) Destroy(gameObject);
+
+		lifeSeconds -= Time.deltaTime;
+	}
+}
