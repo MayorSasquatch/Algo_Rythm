1f3d172f0e6900b56accbaea66dd7883f0e71e0c
OtakuNeko <ryulord1@embarqmail.com>
2014-07-07 00:20:01 -0400

    made songselect better
    yea!

--- Rhythm Siege/Assets/Scripts/SongSelect.cs
+++ Rhythm Siege/Assets/Scripts/SongSelect.cs
@@ -45,9 +45,6 @@ public class SongSelect : MonoBehaviour {
 				"Choose mp3 File",
 				FileSelectedCallback
 				);
-			songBrowser.SelectionPattern = "*.mp3";
-			songBrowser.DirectoryImage = directoryImage;
-			songBrowser.FileImage = fileImage;
 		
 
 	}
@@ -98,65 +95,19 @@ public class FileBrowser {
 	// Called when the user clicks cancel or select
 	public delegate void FinishedCallback(string path);
 	// Defaults to working directory
-	public string CurrentDirectory {
-		get {
-			return m_currentDirectory;
-		}
-		set {
-			SetNewDirectory(value);
-			SwitchDirectoryNow();
-		}
-	}
+
 	protected string m_currentDirectory;
 	// Optional pattern for filtering selectable files/folders. See:
 	// http://msdn.microsoft.com/en-us/library/wz42302f(v=VS.90).aspx
 	// and
 	// http://msdn.microsoft.com/en-us/library/6ff71z1w(v=VS.90).aspx
-	public string SelectionPattern {
-		get {
-			return m_filePattern;
-		}
-		set {
-			m_filePattern = value;
-			ReadDirectoryContents();
-		}
-	}
+
 	protected string m_filePattern;
-	
-	// Optional image for directories
-	public Texture2D DirectoryImage {
-		get {
-			return m_directoryImage;
-		}
-		set {
-			m_directoryImage = value;
-			BuildContent();
-		}
-	}
-	protected Texture2D m_directoryImage;
-	
-	// Optional image for files
-	public Texture2D FileImage {
-		get {
-			return m_fileImage;
-		}
-		set {
-			m_fileImage = value;
-			BuildContent();
-		}
-	}
-	protected Texture2D m_fileImage;
-	
+
+	protected FileInfo[] fileinfo;
+	protected string[] filenames;
 	// Browser type. Defaults to File, but can be set to Folder
-	public FileBrowserType BrowserType {
-		get {
-			return m_browserType;
-		}
-		set {
-			m_browserType = value;
-			ReadDirectoryContents();
-		}
-	}
+
 	protected FileBrowserType m_browserType;
 	protected string m_newDirectory;
 	protected string[] m_currentDirectoryParts;
@@ -201,125 +152,21 @@ public class FileBrowser {
 	public FileBrowser(Rect screenRect, string name, FinishedCallback callback) {
 		m_name = name;
 		m_screenRect = screenRect;
-		m_browserType = FileBrowserType.File;
+		//m_browserType = FileBrowserType.File;
 		m_callback = callback;
-		//Debug.Log (MainMenu.rootfolder);
-		if (MainMenu.rootfolder == "" || MainMenu.rootfolder == null) {
-			SetNewDirectory(Directory.GetCurrentDirectory());}
-			else SetNewDirectory(MainMenu.rootfolder);
 
-		SwitchDirectoryNow();
+		m_newDirectory = "c:"; //Directory.GetCurrentDirectory();
+		DirectoryInfo DR = new DirectoryInfo (m_newDirectory);
+		fileinfo = DR.GetFiles("*mp3", SearchOption.AllDirectories);
+		Array.Sort(fileinfo,(x, y) => StringComparer.OrdinalIgnoreCase.Compare(x.Name, y.Name));
+		filenames = new string[fileinfo.Length];
+		for (int i = 0; i<fileinfo.Length; i++) {
+			filenames[i] = fileinfo[i].Name;
+				}
 	}
 
-	protected void SetNewDirectory(string directory) {
-		m_newDirectory = directory;
-		MainMenu.rootfolder = directory;
-		PlayerPrefs.SetString ("rootfolder", directory);
-	}
-	
-	protected void SwitchDirectoryNow() {
-		if (m_newDirectory == null || m_currentDirectory == m_newDirectory) {
-			return;
-		}
-		m_currentDirectory = m_newDirectory;
-		m_scrollPosition = Vector2.zero;
-		m_selectedDirectory = m_selectedNonMatchingDirectory = m_selectedFile = -1;
-		ReadDirectoryContents();
-	}
-	
-	protected void ReadDirectoryContents() {
-		if (m_currentDirectory == "/") {
-			m_currentDirectoryParts = new string[] {""};
-			m_currentDirectoryMatches = false;
-		} else {
-			m_currentDirectoryParts = m_currentDirectory.Split(Path.DirectorySeparatorChar);
-			if (SelectionPattern != null)
-			{
-				string directoryName = Path.GetDirectoryName(m_currentDirectory);
-				string[] generation = new string[0];
-				if(directoryName != null)
-				{ 	//This is new: generation should be an empty array for the root directory.
-					//directoryName will be null if it's a root directory
-					generation = Directory.GetDirectories(
-						directoryName,
-						SelectionPattern );
-				}
-				m_currentDirectoryMatches = Array.IndexOf(generation, m_currentDirectory) >= 0;
-			}
-			else {
-				m_currentDirectoryMatches = false;
-			}
-		}
 		
-		if (BrowserType == FileBrowserType.File || SelectionPattern == null) {
-			try {m_directories = Directory.GetDirectories(m_currentDirectory);}
-			catch(UnauthorizedAccessException){MainMenu.rootfolder = "";}
-			catch(DirectoryNotFoundException){MainMenu.rootfolder = "";}
 
-			m_nonMatchingDirectories = new string[0];
-		} else {
-			m_directories = Directory.GetDirectories(m_currentDirectory, SelectionPattern);
-			var nonMatchingDirectories = new List<string>();
-			foreach (string directoryPath in Directory.GetDirectories(m_currentDirectory)) {
-				if (Array.IndexOf(m_directories, directoryPath) < 0) {
-					nonMatchingDirectories.Add(directoryPath);
-				}
-			}
-			m_nonMatchingDirectories = nonMatchingDirectories.ToArray();
-			for (int i = 0; i < m_nonMatchingDirectories.Length; ++i) {
-				int lastSeparator = m_nonMatchingDirectories[i].LastIndexOf(Path.DirectorySeparatorChar);
-				m_nonMatchingDirectories[i] = m_nonMatchingDirectories[i].Substring(lastSeparator + 1);
-			}
-			Array.Sort(m_nonMatchingDirectories);
-		}
-		
-		for (int i = 0; i < m_directories.Length; ++i) {
-			m_directories[i] = m_directories[i].Substring(m_directories[i].LastIndexOf(Path.DirectorySeparatorChar) + 1);
-		}
-		
-		if (BrowserType == FileBrowserType.Directory || SelectionPattern == null) {
-			m_files = Directory.GetFiles(m_currentDirectory);
-			m_nonMatchingFiles = new string[0];
-		} else {
-			m_files = Directory.GetFiles(m_currentDirectory, SelectionPattern);
-			var nonMatchingFiles = new List<string>();
-			foreach (string filePath in Directory.GetFiles(m_currentDirectory)) {
-				if (Array.IndexOf(m_files, filePath) < 0) {
-					nonMatchingFiles.Add(filePath);
-				}
-			}
-			m_nonMatchingFiles = nonMatchingFiles.ToArray();
-			for (int i = 0; i < m_nonMatchingFiles.Length; ++i) {
-				m_nonMatchingFiles[i] = Path.GetFileName(m_nonMatchingFiles[i]);
-			}
-			Array.Sort(m_nonMatchingFiles);
-		}
-		for (int i = 0; i < m_files.Length; ++i) {
-			m_files[i] = Path.GetFileName(m_files[i]);
-		}
-		Array.Sort(m_files);
-		BuildContent();
-		m_newDirectory = null;
-	}
-	
-	protected void BuildContent() {
-		m_directoriesWithImages = new GUIContent[m_directories.Length];
-		for (int i = 0; i < m_directoriesWithImages.Length; ++i) {
-			m_directoriesWithImages[i] = new GUIContent(m_directories[i], DirectoryImage);
-		}
-		m_nonMatchingDirectoriesWithImages = new GUIContent[m_nonMatchingDirectories.Length];
-		for (int i = 0; i < m_nonMatchingDirectoriesWithImages.Length; ++i) {
-			m_nonMatchingDirectoriesWithImages[i] = new GUIContent(m_nonMatchingDirectories[i], DirectoryImage);
-		}
-		m_filesWithImages = new GUIContent[m_files.Length];
-		for (int i = 0; i < m_filesWithImages.Length; ++i) {
-			m_filesWithImages[i] = new GUIContent(m_files[i], FileImage);
-		}
-		m_nonMatchingFilesWithImages = new GUIContent[m_nonMatchingFiles.Length];
-		for (int i = 0; i < m_nonMatchingFilesWithImages.Length; ++i) {
-			m_nonMatchingFilesWithImages[i] = new GUIContent(m_nonMatchingFiles[i], FileImage);
-		}
-	}
 	
 	public void OnGUI() {
 		GUILayout.BeginArea(
@@ -327,20 +174,7 @@ public class FileBrowser {
 			m_name,
 			GUI.skin.window
 			);
-		GUILayout.BeginHorizontal();
-		for (int parentIndex = 0; parentIndex < m_currentDirectoryParts.Length; ++parentIndex) {
-			if (parentIndex == m_currentDirectoryParts.Length - 1) {
-				GUILayout.Label(m_currentDirectoryParts[parentIndex], CentredText,GUILayout.Width(Screen.width*.1f),GUILayout.Height(Screen.height*.1f));
-			} else if (GUILayout.Button(m_currentDirectoryParts[parentIndex],GUILayout.Width(Screen.width*.1f),GUILayout.Height(Screen.height*.1f))) {
-				string parentDirectoryName = m_currentDirectory;
-				for (int i = m_currentDirectoryParts.Length - 1; i > parentIndex; --i) {
-					parentDirectoryName = Path.GetDirectoryName(parentDirectoryName);
-				}
-				SetNewDirectory(parentDirectoryName);
-			}
-		}
-		GUILayout.FlexibleSpace();
-		GUILayout.EndHorizontal();
+
 		m_scrollPosition = GUILayout.BeginScrollView(
 			m_scrollPosition,
 			false,
@@ -349,96 +183,40 @@ public class FileBrowser {
 			GUI.skin.verticalScrollbar,
 			GUI.skin.box
 			);
-		m_selectedDirectory = GUILayoutx.SelectionList(
-			m_selectedDirectory,
-			m_directoriesWithImages,
-			DirectoryDoubleClickCallback
-			);
-		if (m_selectedDirectory > -1) {
-			m_selectedFile = m_selectedNonMatchingDirectory = -1;
-		}
-		m_selectedNonMatchingDirectory = GUILayoutx.SelectionList(
-			m_selectedNonMatchingDirectory,
-			m_nonMatchingDirectoriesWithImages,
-			NonMatchingDirectoryDoubleClickCallback
-			);
-		if (m_selectedNonMatchingDirectory > -1) {
-			m_selectedDirectory = m_selectedFile = -1;
-		}
-		GUI.enabled = BrowserType == FileBrowserType.File;
+
 		m_selectedFile = GUILayoutx.SelectionList(
 			m_selectedFile,
-			m_filesWithImages,
+			filenames,
 			FileDoubleClickCallback
 			);
-		GUI.enabled = true;
-		if (m_selectedFile > -1) {
-			m_selectedDirectory = m_selectedNonMatchingDirectory = -1;
-		}
-		GUI.enabled = false;
-		GUILayoutx.SelectionList(
-			-1,
-			m_nonMatchingFilesWithImages
-			);
-		GUI.enabled = true;
+
 		GUILayout.EndScrollView();
 		GUILayout.BeginHorizontal();
 		GUILayout.FlexibleSpace();
 		if (GUILayout.Button("Cancel", GUILayout.Width(Screen.width*.4f),GUILayout.Height(Screen.height*.1f))) {
 			m_callback(null);
 		}
-		if (BrowserType == FileBrowserType.File) {
-			GUI.enabled = m_selectedFile > -1;
-		} else {
-			if (SelectionPattern == null) {
-				GUI.enabled = m_selectedDirectory > -1;
-			} else {
-				GUI.enabled =	m_selectedDirectory > -1 ||
-					(
-						m_currentDirectoryMatches &&
-						m_selectedNonMatchingDirectory == -1 &&
-						m_selectedFile == -1
-						);
-			}
-		}
+
 		if (GUILayout.Button("Select", GUILayout.Width(Screen.width*.4f),GUILayout.Height(Screen.height*.1f))) {
-			if (BrowserType == FileBrowserType.File) {
-				m_callback(Path.Combine(m_currentDirectory, m_files[m_selectedFile]));
-			} else {
-				if (m_selectedDirectory > -1) {
-					m_callback(Path.Combine(m_currentDirectory, m_directories[m_selectedDirectory]));
-				} else {
-					m_callback(m_currentDirectory);
-				}
-			}
+			m_callback( fileinfo[m_selectedFile].FullName);	
 		}
 		GUI.enabled = true;
 		GUILayout.EndHorizontal();
 		GUILayout.EndArea();
-		
-		if (Event.current.type == EventType.Repaint) {
-			SwitchDirectoryNow();
-		}
+
 	}
 	public void Scroll(Vector2 scroll ){
 		m_scrollPosition += scroll;
 	}
 
 	protected void FileDoubleClickCallback(int i) {
-		if (BrowserType == FileBrowserType.File) {
-			m_callback(Path.Combine(m_currentDirectory, m_files[i]));
-		}
+		m_callback( fileinfo[m_selectedFile].FullName);
 	}
-	
-	protected void DirectoryDoubleClickCallback(int i) {
-		SetNewDirectory(Path.Combine(m_currentDirectory, m_directories[i]));
-	}
-	
-	protected void NonMatchingDirectoryDoubleClickCallback(int i) {
-		SetNewDirectory(Path.Combine(m_currentDirectory, m_nonMatchingDirectories[i]));
-	}
-	
+
 }
+
+
+
 // selected list class
 public class GUILayoutx {
 	
